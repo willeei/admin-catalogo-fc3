5dc7c41d1676ffb33742d8df8615158fc6d4ba17
Williams Barriquero <williamsgomess@outlook.com>
2023-10-05 01:05:25 -0300

    Tornando a classe category um AggregateRoot

--- domain/src/main/java/br/com/williamsbarriquero/admin/catalogo/domain/category/Category.java
+++ domain/src/main/java/br/com/williamsbarriquero/admin/catalogo/domain/category/Category.java
@@ -1,11 +1,12 @@
 package br.com.williamsbarriquero.admin.catalogo.domain.category;
 
+import br.com.williamsbarriquero.admin.catalogo.domain.AggregateRoot;
+
 import java.time.Instant;
 import java.util.UUID;
 
-public class Category {
+public class Category extends AggregateRoot<CategoryID> {
 
-    private String id;
     private String name;
     private String description;
     private boolean active;
@@ -15,30 +16,30 @@ public class Category {
 
 
     private Category(
-            final String id,
-            final String name,
-            final String description,
-            final boolean active,
-            final Instant createdAt,
-            final Instant updatedAt,
-            final Instant deletedAt
+            final CategoryID anId,
+            final String aName,
+            final String aDescription,
+            final boolean isActive,
+            final Instant aCreatedAt,
+            final Instant aUpdatedAt,
+            final Instant aDeletedAt
     ) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.active = active;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.deletedAt = deletedAt;
+        super(anId);
+        this.name = aName;
+        this.description = aDescription;
+        this.active = isActive;
+        this.createdAt = aCreatedAt;
+        this.updatedAt = aUpdatedAt;
+        this.deletedAt = aDeletedAt;
     }
 
     public static Category newCategory(final String aName, String aDescription, boolean isActive) {
-        final var id = UUID.randomUUID().toString();
+        final var id = CategoryID.unique();
         final Instant now = Instant.now();
         return new Category(id, aName, aDescription, isActive, now, now, null);
     }
 
-    public String getId() {
+    public CategoryID getId() {
         return id;
     }
 
---
+++ domain/src/main/java/br/com/williamsbarriquero/admin/catalogo/domain/category/CategoryID.java
@@ -0,0 +1,45 @@
+package br.com.williamsbarriquero.admin.catalogo.domain.category;
+
+import br.com.williamsbarriquero.admin.catalogo.domain.Identifier;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class CategoryID extends Identifier {
+
+    private final String value;
+
+    private CategoryID(final String value) {
+        Objects.requireNonNull(value);
+        this.value = value;
+    }
+
+    public static CategoryID unique() {
+        return CategoryID.from(UUID.randomUUID().toString().toLowerCase());
+    }
+
+    public static CategoryID from(final String anId) {
+        return new CategoryID(anId);
+    }
+
+    public static CategoryID from(final UUID anId) {
+        return new CategoryID(anId.randomUUID().toString());
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final CategoryID that = (CategoryID) o;
+        return Objects.equals(getValue(), that.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getValue());
+    }
+}
