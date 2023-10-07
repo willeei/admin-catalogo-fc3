0a6cc539468eca88f6f19e6aa7e9e08971776edd
Williams Barriquero <williamsgomess@outlook.com>
2023-10-07 15:13:29 -0300

    Testes unitários do caso de uso de criação de categoria

--- application/src/test/java/br/com/williamsbarriquero/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.java
+++ application/src/test/java/br/com/williamsbarriquero/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.java
@@ -1,5 +1,43 @@
 package br.com.williamsbarriquero.admin.catalogo.application.category.create;
 
+import br.com.williamsbarriquero.admin.catalogo.domain.category.CategoryGateway;
+import org.junit.jupiter.api.Assertions;
+import org.mockito.Mockito;
+
+import java.util.Objects;
+
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+
 public class CreateCategoryUseCaseTest {
+
+    public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais essistida";
+        final var expectedIsActive = true;
+
+        final var aCommand = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        final CategoryGateway categoryGateway = Mockito.mock(CategoryGateway.class);
+
+        Mockito.when(categoryGateway.create(Mockito.any()))
+                .thenAnswer(returnsFirstArg());
+
+        final var useCase = new CreateCategoryUseCase(categoryGateway);
+
+        final var actualOutput = useCase.execute(aCommand);
+
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertNotNull(actualOutput.getId());
+
+        Mockito.verify(categoryGateway, Mockito.times(1))
+                .create(Mockito.argThat(aCategory ->
+                        Objects.equals(expectedName, aCategory.getName())
+                                && Objects.equals(expectedDescription, aCategory.getDescription())
+                                && Objects.equals(expectedIsActive, aCategory.isActive())
+                                && Objects.nonNull(aCategory.getId())
+                                && Objects.nonNull(aCategory.getCreatedAt())
+                                && Objects.nonNull(aCategory.getUpdatedAt())
+                                && Objects.isNull(aCategory.getDeletedAt())))
+    }
 
 }
