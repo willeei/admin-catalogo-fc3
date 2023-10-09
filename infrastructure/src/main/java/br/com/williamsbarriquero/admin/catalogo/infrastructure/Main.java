223afe75b66e610ff261e93cdae4f25c6bc91103
Williams Barriquero <williamsgomess@outlook.com>
2023-10-09 00:15:21 -0300

    Adicionando Spring Boot na camada de infrastructure

--- infrastructure/src/main/java/br/com/williamsbarriquero/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/br/com/williamsbarriquero/admin/catalogo/infrastructure/Main.java
@@ -1,10 +1,13 @@
 package br.com.williamsbarriquero.admin.catalogo.infrastructure;
 
-import br.com.williamsbarriquero.admin.catalogo.application.UseCase;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println(new UseCase().execute());
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
---
+++ infrastructure/src/main/java/br/com/williamsbarriquero/admin/catalogo/infrastructure/WebServerConfig.java
@@ -0,0 +1,10 @@
+package br.com.williamsbarriquero.admin.catalogo.infrastructure;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("br.com.williamsbarriquero.admin.catalogo")
+public class WebServerConfig {
+
+}
