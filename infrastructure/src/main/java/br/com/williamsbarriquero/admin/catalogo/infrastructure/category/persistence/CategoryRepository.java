a79de013a5279bfd93b2cac9a542aade9e834bb3
Williams Barriquero <williamsgomess@outlook.com>
2023-10-17 21:50:57 -0300

    Criando o jpa repository da categoria

---
+++ infrastructure/src/main/java/br/com/williamsbarriquero/admin/catalogo/infrastructure/category/persistence/CategoryRepository.java
@@ -0,0 +1,7 @@
+package br.com.williamsbarriquero.admin.catalogo.infrastructure.category.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<CategoryJpaEntity, String> {
+
+}
