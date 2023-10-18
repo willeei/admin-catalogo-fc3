01e6efcf53cd8e9a3780a6c2980fabd7238fae83
Williams Barriquero <williamsgomess@outlook.com>
2023-10-17 21:57:01 -0300

    Criando a implementação do gateway de categoria

---
+++ infrastructure/src/main/java/br/com/williamsbarriquero/admin/catalogo/infrastructure/category/persistence/CategoryMySQLGateway.java
@@ -0,0 +1,45 @@
+package br.com.williamsbarriquero.admin.catalogo.infrastructure.category.persistence;
+
+import br.com.williamsbarriquero.admin.catalogo.domain.category.Category;
+import br.com.williamsbarriquero.admin.catalogo.domain.category.CategoryGateway;
+import br.com.williamsbarriquero.admin.catalogo.domain.category.CategoryID;
+import br.com.williamsbarriquero.admin.catalogo.domain.category.CategorySearchQuery;
+import br.com.williamsbarriquero.admin.catalogo.domain.pagination.Pagination;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoryMySQLGateway implements CategoryGateway {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryMySQLGateway(final CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public Category create(Category aCategory) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(CategoryID anId) {
+
+    }
+
+    @Override
+    public Optional<Category> findById(CategoryID anId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Category update(Category aCategory) {
+        return null;
+    }
+
+    @Override
+    public Pagination<Category> findAll(CategorySearchQuery aQuery) {
+        return null;
+    }
+}
