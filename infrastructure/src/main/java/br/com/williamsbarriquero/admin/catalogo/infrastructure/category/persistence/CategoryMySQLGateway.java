6968b78df031279e8bfaf5906776e0f280593bcf
Williams Barriquero <williamsgomess@outlook.com>
2023-10-20 01:09:30 -0300

    Implementando o método update do gateway de categoria

--- infrastructure/src/main/java/br/com/williamsbarriquero/admin/catalogo/infrastructure/category/persistence/CategoryMySQLGateway.java
+++ infrastructure/src/main/java/br/com/williamsbarriquero/admin/catalogo/infrastructure/category/persistence/CategoryMySQLGateway.java
@@ -20,7 +20,7 @@ public class CategoryMySQLGateway implements CategoryGateway {
 
     @Override
     public Category create(final Category aCategory) {
-        return this.categoryRepository.save(CategoryJpaEntity.from(aCategory)).toAggregate();
+        return save(aCategory);
     }
 
     @Override
@@ -34,12 +34,16 @@ public class CategoryMySQLGateway implements CategoryGateway {
     }
 
     @Override
-    public Category update(Category aCategory) {
-        return null;
+    public Category update(final Category aCategory) {
+        return save(aCategory);
     }
 
     @Override
     public Pagination<Category> findAll(CategorySearchQuery aQuery) {
         return null;
+    }
+
+    private Category save(final Category aCategory) {
+        return this.categoryRepository.save(CategoryJpaEntity.from(aCategory)).toAggregate();
     }
 }
