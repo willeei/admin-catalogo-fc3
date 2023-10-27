9a9c5caaf3611960f79a83c92bbb88aa128ecb75
Williams Barriquero <williamsgomess@outlook.com>
2023-10-27 10:16:20 -0300

    Criando a estrategia para testes integrados de controller

--- infrastructure/src/main/java/br/com/williamsbarriquero/admin/catalogo/infrastructure/api/controller/CategoryController.java
+++ infrastructure/src/main/java/br/com/williamsbarriquero/admin/catalogo/infrastructure/api/controller/CategoryController.java
@@ -1,12 +1,21 @@
 package br.com.williamsbarriquero.admin.catalogo.infrastructure.api.controller;
 
+import br.com.williamsbarriquero.admin.catalogo.application.category.create.CreateCategoryUseCase;
 import br.com.williamsbarriquero.admin.catalogo.domain.pagination.Pagination;
 import br.com.williamsbarriquero.admin.catalogo.infrastructure.api.CategoryAPI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 public class CategoryController implements CategoryAPI {
+
+    private final CreateCategoryUseCase createCategoryUseCase;
+
+    public CategoryController(final CreateCategoryUseCase createCategoryUseCase) {
+        this.createCategoryUseCase = Objects.requireNonNull(createCategoryUseCase);
+    }
 
     @Override
     public ResponseEntity<?> createCategory() {
