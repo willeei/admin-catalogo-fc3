332e78bc21a5f7bd548fe2c1cd7440a76bdfa76f
Williams Barriquero <williamsgomess@outlook.com>
2023-11-22 20:52:45 -0300

    Testes de desserialização da classe createcategoryrequest

---
+++ infrastructure/src/test/java/br/com/williamsbarriquero/admin/catalogo/infrastructure/category/models/CreateCategoryRequestTest.java
@@ -0,0 +1,61 @@
+package br.com.williamsbarriquero.admin.catalogo.infrastructure.category.models;
+
+import br.com.williamsbarriquero.admin.catalogo.JacksonTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+@JacksonTest
+class CreateCategoryRequestTest {
+
+    @Autowired
+    private JacksonTester<CreateCategoryRequest> json;
+
+    @Test
+    void testMarchall() throws Exception {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = false;
+
+        final var response = new CreateCategoryRequest(
+                expectedName,
+                expectedDescription,
+                expectedIsActive
+        );
+
+        final var actualJson = this.json.write(response);
+
+        Assertions.assertThat(actualJson)
+                .hasJsonPathValue("$.name", expectedName)
+                .hasJsonPathValue("$.description", expectedDescription)
+                .hasJsonPathValue("$.is_active", expectedIsActive);
+    }
+
+    @Test
+    void testUnmarshall() throws Exception {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var json = """
+                {
+                  "name": "%s",
+                  "description": "%s",
+                  "is_active": %s
+                }
+                """.formatted(
+                expectedName,
+                expectedDescription,
+                expectedIsActive
+        );
+
+        final var actualJson = this.json.parse(json);
+
+        Assertions.assertThat(actualJson)
+                .hasFieldOrPropertyWithValue("name", expectedName)
+                .hasFieldOrPropertyWithValue("description", expectedDescription)
+                .hasFieldOrPropertyWithValue("active", expectedIsActive);
+    }
+
+}
