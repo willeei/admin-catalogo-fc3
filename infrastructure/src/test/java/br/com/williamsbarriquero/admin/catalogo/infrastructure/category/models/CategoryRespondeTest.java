94eef04446ff488f52cc155cd6ab284c5fc44482
Williams Barriquero <williamsgomess@outlook.com>
2023-11-22 19:32:46 -0300

    Criando a estrategia para testes integrados de serialização

---
+++ infrastructure/src/test/java/br/com/williamsbarriquero/admin/catalogo/infrastructure/category/models/CategoryRespondeTest.java
@@ -0,0 +1,8 @@
+package br.com.williamsbarriquero.admin.catalogo.infrastructure.category.models;
+
+import br.com.williamsbarriquero.admin.catalogo.JacksonTest;
+
+@JacksonTest
+public class CategoryRespondeTest {
+
+}
