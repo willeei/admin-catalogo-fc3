e21e8ab914f8cb68d4af3023dcb106ddcf766129
Williams Barriquero <williamsgomess@outlook.com>
2023-11-21 22:45:21 -0300

    Testes integrados do endpoint de listagem de categoria

--- infrastructure/src/test/java/br/com/williamsbarriquero/admin/catalogo/infrastructure/api/CategoryAPITest.java
+++ infrastructure/src/test/java/br/com/williamsbarriquero/admin/catalogo/infrastructure/api/CategoryAPITest.java
@@ -6,12 +6,15 @@ import br.com.williamsbarriquero.admin.catalogo.application.category.create.Crea
 import br.com.williamsbarriquero.admin.catalogo.application.category.delete.DeleteCategoryUseCase;
 import br.com.williamsbarriquero.admin.catalogo.application.category.retrieve.get.CategoryOutput;
 import br.com.williamsbarriquero.admin.catalogo.application.category.retrieve.get.GetCategoryByIdUseCase;
+import br.com.williamsbarriquero.admin.catalogo.application.category.retrieve.list.CategoryListOutput;
+import br.com.williamsbarriquero.admin.catalogo.application.category.retrieve.list.ListCategoriesUseCase;
 import br.com.williamsbarriquero.admin.catalogo.application.category.update.UpdateCategoryOutput;
 import br.com.williamsbarriquero.admin.catalogo.application.category.update.UpdateCategoryUseCase;
 import br.com.williamsbarriquero.admin.catalogo.domain.category.Category;
 import br.com.williamsbarriquero.admin.catalogo.domain.category.CategoryID;
 import br.com.williamsbarriquero.admin.catalogo.domain.exceptions.DomainException;
 import br.com.williamsbarriquero.admin.catalogo.domain.exceptions.NotFoundException;
+import br.com.williamsbarriquero.admin.catalogo.domain.pagination.Pagination;
 import br.com.williamsbarriquero.admin.catalogo.domain.validation.Error;
 import br.com.williamsbarriquero.admin.catalogo.domain.validation.handler.Notification;
 import br.com.williamsbarriquero.admin.catalogo.infrastructure.category.models.CreateCategoryApiInput;
@@ -23,6 +26,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
 import java.util.Objects;
 
 import static io.vavr.API.Left;
@@ -54,6 +58,9 @@ class CategoryAPITest {
 
     @MockBean
     private DeleteCategoryUseCase deleteCategoryUseCase;
+
+    @MockBean
+    private ListCategoriesUseCase listCategoriesUseCase;
 
     @Test
     void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() throws Exception {
@@ -351,60 +358,60 @@ class CategoryAPITest {
         // then
         response.andExpect(status().isNoContent());
 
-        verify(deleteCategoryUseCase, times(1)).execute(eq(expectedId));
+        verify(deleteCategoryUseCase, times(1)).execute(expectedId);
     }
-//
-//    @Test
-//    void givenValidParams_whenCallsListCategories_shouldReturnCategories() throws Exception {
-//        // given
-//        final var aCategory = Category.newCategory("Movies", null, true);
-//
-//        final var expectedPage = 0;
-//        final var expectedPerPage = 10;
-//        final var expectedTerms = "movies";
-//        final var expectedSort = "description";
-//        final var expectedDirection = "desc";
-//        final var expectedItemsCount = 1;
-//        final var expectedTotal = 1;
-//
-//        final var expectedItems = List.of(CategoryListOutput.from(aCategory));
-//
-//        when(listCategoriesUseCase.execute(any()))
-//                .thenReturn(new Pagination<>(expectedPage, expectedPerPage, expectedTotal, expectedItems));
-//
-//        // when
-//        final var request = get("/categories")
-//                .with(ApiTest.CATEGORIES_JWT)
-//                .queryParam("page", String.valueOf(expectedPage))
-//                .queryParam("perPage", String.valueOf(expectedPerPage))
-//                .queryParam("sort", expectedSort)
-//                .queryParam("dir", expectedDirection)
-//                .queryParam("search", expectedTerms)
-//                .accept(MediaType.APPLICATION_JSON)
-//                .contentType(MediaType.APPLICATION_JSON);
-//
-//        final var response = this.mvc.perform(request)
-//                .andDo(print());
-//
-//        // then
-//        response.andExpect(status().isOk())
-//                .andExpect(jsonPath("$.current_page", equalTo(expectedPage)))
-//                .andExpect(jsonPath("$.per_page", equalTo(expectedPerPage)))
-//                .andExpect(jsonPath("$.total", equalTo(expectedTotal)))
-//                .andExpect(jsonPath("$.items", hasSize(expectedItemsCount)))
-//                .andExpect(jsonPath("$.items[0].id", equalTo(aCategory.getId().getValue())))
-//                .andExpect(jsonPath("$.items[0].name", equalTo(aCategory.getName())))
-//                .andExpect(jsonPath("$.items[0].description", equalTo(aCategory.getDescription())))
-//                .andExpect(jsonPath("$.items[0].is_active", equalTo(aCategory.isActive())))
-//                .andExpect(jsonPath("$.items[0].created_at", equalTo(aCategory.getCreatedAt().toString())))
-//                .andExpect(jsonPath("$.items[0].deleted_at", equalTo(aCategory.getDeletedAt())));
-//
-//        verify(listCategoriesUseCase, times(1)).execute(argThat(query ->
-//                Objects.equals(expectedPage, query.page())
-//                        && Objects.equals(expectedPerPage, query.perPage())
-//                        && Objects.equals(expectedDirection, query.direction())
-//                        && Objects.equals(expectedSort, query.sort())
-//                        && Objects.equals(expectedTerms, query.terms())
-//        ));
-//    }
+
+    @Test
+    void givenValidParams_whenCallsListCategories_shouldReturnCategories() throws Exception {
+        // given
+        final var aCategory = Category.newCategory("Movies", null, true);
+
+        final var expectedPage = 0;
+        final var expectedPerPage = 10;
+        final var expectedTerms = "movies";
+        final var expectedSort = "description";
+        final var expectedDirection = "desc";
+        final var expectedItemsCount = 1;
+        final var expectedTotal = 1;
+
+        final var expectedItems = List.of(CategoryListOutput.from(aCategory));
+
+        when(listCategoriesUseCase.execute(any()))
+                .thenReturn(new Pagination<>(expectedPage, expectedPerPage, expectedTotal, expectedItems));
+
+        // when
+        final var request = get("/categories")
+                .queryParam("page", String.valueOf(expectedPage))
+                .queryParam("perPage", String.valueOf(expectedPerPage))
+                .queryParam("sort", expectedSort)
+                .queryParam("dir", expectedDirection)
+                .queryParam("search", expectedTerms)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        final var response = this.mvc.perform(request)
+                .andDo(print());
+
+        // then
+        response.andExpect(status().isOk())
+                .andExpect(jsonPath("$.current_page", equalTo(expectedPage)))
+                .andExpect(jsonPath("$.per_page", equalTo(expectedPerPage)))
+                .andExpect(jsonPath("$.total", equalTo(expectedTotal)))
+                .andExpect(jsonPath("$.items", hasSize(expectedItemsCount)))
+                .andExpect(jsonPath("$.items[0].id", equalTo(aCategory.getId().getValue())))
+                .andExpect(jsonPath("$.items[0].name", equalTo(aCategory.getName())))
+                .andExpect(jsonPath("$.items[0].description", equalTo(aCategory.getDescription())))
+                .andExpect(jsonPath("$.items[0].is_active", equalTo(aCategory.isActive())))
+                .andExpect(jsonPath("$.items[0].created_at", equalTo(aCategory.getCreatedAt().toString())))
+                .andExpect(jsonPath("$.items[0].deleted_at", equalTo(aCategory.getDeletedAt())));
+
+        verify(listCategoriesUseCase, times(1)).execute(argThat(query ->
+                Objects.equals(expectedPage, query.page())
+                        && Objects.equals(expectedPerPage, query.perPage())
+                        && Objects.equals(expectedDirection, query.direction())
+                        && Objects.equals(expectedSort, query.sort())
+                        && Objects.equals(expectedTerms, query.terms())
+        ));
+    }
+
 }
