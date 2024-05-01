4e1aa2f1fb1bf4fe10f76b40c11e16f380e179a5
Williams Barriquero <williamsgomess@outlook.com>
2024-05-01 12:06:49 -0300

    Testes do comportamento de update do genre

--- domain/src/main/java/br/com/williamsbarriquero/admin/catalogo/domain/exceptions/NoStacktraceException.java
+++ domain/src/main/java/br/com/williamsbarriquero/admin/catalogo/domain/exceptions/NoStacktraceException.java
@@ -1,6 +1,11 @@
 package br.com.williamsbarriquero.admin.catalogo.domain.exceptions;
 
+import java.io.Serial;
+
 public class NoStacktraceException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = -6836572721387793183L;
 
     public NoStacktraceException(final String message) {
         this(message, null);
