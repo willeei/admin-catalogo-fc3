b036537a0730ea9783a8d1dbf0828645446195c8
Williams Barriquero <williamsgomess@outlook.com>
2024-05-19 02:09:29 -0300

    Usecases de Genre - ajuste
    Implementando o caso de uso de atualização de genre

--- application/src/test/java/br/com/williamsbarriquero/admin/catalogo/application/genre/update/UpdateGenreUseCaseTest.java
+++ application/src/test/java/br/com/williamsbarriquero/admin/catalogo/application/genre/update/UpdateGenreUseCaseTest.java
@@ -20,7 +20,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class UpdateGenreUseCase {
+class UpdateGenreUseCaseTest {
 
     @InjectMocks
     private DefaultUpdateGenreUseCase useCase;
