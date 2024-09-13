3d2548f2dd9bd4d86d5b1ccbaaa10614bf1cb502
Williams Barriquero <willbrr.dev@gmail.com>
2024-09-13 12:00:47 -0300

    feat: :wrench: configura gcp

---
+++ infrastructure/src/main/java/tech/willeei/admin/catalogo/infrastructure/configuration/GoogleCloudConfig.java
@@ -0,0 +1,70 @@
+package tech.willeei.admin.catalogo.infrastructure.configuration;
+
+import com.google.api.gax.retrying.RetrySettings;
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.http.HttpTransportOptions;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.threeten.bp.Duration;
+import tech.willeei.admin.catalogo.infrastructure.configuration.properties.GoogleCloudProperties;
+import tech.willeei.admin.catalogo.infrastructure.configuration.properties.GoogleStorageProperties;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Base64;
+
+@Configuration
+@Profile({"development", "production"})
+public class GoogleCloudConfig {
+
+    @Bean
+    @ConfigurationProperties("google.cloud")
+    public GoogleCloudProperties googleCloudProperties() {
+        return new GoogleCloudProperties();
+    }
+
+    @Bean
+    @ConfigurationProperties("google.cloud.storage.catalogo-video")
+    public GoogleStorageProperties googleStorageProperties() {
+        return new GoogleStorageProperties();
+    }
+
+    @Bean
+    public Credentials credentials(final GoogleCloudProperties props) {
+        final var jsonContent = Base64.getDecoder().decode(props.getCredentials());
+
+        try (final var stream = new ByteArrayInputStream(jsonContent)) {
+            return GoogleCredentials.fromStream(stream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Bean
+    public Storage storage(final Credentials credentials, final GoogleStorageProperties storageProperties) {
+        final var transportOptions = HttpTransportOptions.newBuilder()
+                .setConnectTimeout(storageProperties.getConnectionTimeout())
+                .setReadTimeout(storageProperties.getReadTimeout())
+                .build();
+
+        final var retrySettings = RetrySettings.newBuilder()
+                .setInitialRetryDelay(Duration.ofMillis(storageProperties.getRetryDelay()))
+                .setMaxRetryDelay(Duration.ofMillis(storageProperties.getRetryMaxDelay()))
+                .setMaxAttempts(storageProperties.getRetryMaxAttempts())
+                .setRetryDelayMultiplier(storageProperties.getRetryMultiplier())
+                .build();
+
+        final var options = StorageOptions.newBuilder()
+                .setCredentials(credentials)
+                .setRetrySettings(retrySettings)
+                .setTransportOptions(transportOptions)
+                .build();
+
+        return options.getService();
+    }
+}
---
+++ infrastructure/src/main/java/tech/willeei/admin/catalogo/infrastructure/configuration/properties/GoogleStorageProperties.java
@@ -0,0 +1,99 @@
+package tech.willeei.admin.catalogo.infrastructure.configuration.properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+
+public class GoogleStorageProperties implements InitializingBean {
+
+    private static final Logger log = LoggerFactory.getLogger(GoogleStorageProperties.class);
+
+    private String bucket;
+    private int connectionTimeout;
+    private int readTimeout;
+    private int retryDelay;
+    private int retryMaxAttempts;
+    private int retryMaxDelay;
+    private double retryMultiplier;
+
+    public String getBucket() {
+        return bucket;
+    }
+
+    public GoogleStorageProperties setBucket(String bucket) {
+        this.bucket = bucket;
+        return this;
+    }
+
+    public int getConnectionTimeout() {
+        return connectionTimeout;
+    }
+
+    public GoogleStorageProperties setConnectionTimeout(int connectionTimeout) {
+        this.connectionTimeout = connectionTimeout;
+        return this;
+    }
+
+    public int getReadTimeout() {
+        return readTimeout;
+    }
+
+    public GoogleStorageProperties setReadTimeout(int readTimeout) {
+        this.readTimeout = readTimeout;
+        return this;
+    }
+
+    public int getRetryDelay() {
+        return retryDelay;
+    }
+
+    public GoogleStorageProperties setRetryDelay(int retryDelay) {
+        this.retryDelay = retryDelay;
+        return this;
+    }
+
+    public int getRetryMaxAttempts() {
+        return retryMaxAttempts;
+    }
+
+    public GoogleStorageProperties setRetryMaxAttempts(int retryMaxAttempts) {
+        this.retryMaxAttempts = retryMaxAttempts;
+        return this;
+    }
+
+    public int getRetryMaxDelay() {
+        return retryMaxDelay;
+    }
+
+    public GoogleStorageProperties setRetryMaxDelay(int retryMaxDelay) {
+        this.retryMaxDelay = retryMaxDelay;
+        return this;
+    }
+
+    public double getRetryMultiplier() {
+        return retryMultiplier;
+    }
+
+    public GoogleStorageProperties setRetryMultiplier(double retryMultiplier) {
+        this.retryMultiplier = retryMultiplier;
+        return this;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.debug(toString());
+    }
+
+    @Override
+    public String toString() {
+        return "GoogleStorageProperties{" +
+                "bucket='" + bucket + '\'' +
+                ", connectionTimeout=" + connectionTimeout +
+                ", readTimeout=" + readTimeout +
+                ", retryDelay=" + retryDelay +
+                ", retryMaxAttempts=" + retryMaxAttempts +
+                ", retryMaxDelay=" + retryMaxDelay +
+                ", retryMultiplier=" + retryMultiplier +
+                '}';
+    }
+}
