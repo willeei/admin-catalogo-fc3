020f21f17e6c539f4c137f2002b23ab60b452302
Williams Barriquero <willbrr.dev@gmail.com>
2024-09-19 00:45:42 -0300

    feat: :sparkles: qualificando os beans para injeção do spring

--- infrastructure/src/main/java/tech/willeei/admin/catalogo/infrastructure/configuration/AmpqConfig.java
+++ infrastructure/src/main/java/tech/willeei/admin/catalogo/infrastructure/configuration/AmpqConfig.java
@@ -3,6 +3,8 @@ package tech.willeei.admin.catalogo.infrastructure.configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import tech.willeei.admin.catalogo.infrastructure.configuration.annotations.VideoCreatedQueue;
+import tech.willeei.admin.catalogo.infrastructure.configuration.annotations.VideoEncodedQueue;
 import tech.willeei.admin.catalogo.infrastructure.configuration.properties.amqp.QueueProperties;
 
 @Configuration
@@ -10,13 +12,20 @@ public class AmpqConfig {
 
     @Bean
     @ConfigurationProperties("amqp.queues.video-created")
+    @VideoCreatedQueue
     public QueueProperties videoCreatedQueueProperties() {
         return new QueueProperties();
     }
 
     @Bean
     @ConfigurationProperties("amqp.queues.video-encoded")
+    @VideoEncodedQueue
     public QueueProperties encodedCreatedQueueProperties() {
         return new QueueProperties();
+    }
+
+    @Bean
+    public String queueName(@VideoCreatedQueue QueueProperties props) {
+        return props.getQueue();
     }
 }
