1ac95e20004e59e14a56367e8cb418f3778189dd
Williams Barriquero <willbrr.dev@gmail.com>
2024-09-23 07:50:33 -0300

    feat: :sparkles: criando o amqp listener de encoder de video

---
+++ infrastructure/src/main/java/tech/willeei/admin/catalogo/infrastructure/amqp/VideoEncoderListener.java
@@ -0,0 +1,49 @@
+package tech.willeei.admin.catalogo.infrastructure.amqp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import tech.willeei.admin.catalogo.application.video.media.update.UpdateMediaStatusCommand;
+import tech.willeei.admin.catalogo.application.video.media.update.UpdateMediaStatusUseCase;
+import tech.willeei.admin.catalogo.domain.video.MediaStatus;
+import tech.willeei.admin.catalogo.infrastructure.configuration.json.Json;
+import tech.willeei.admin.catalogo.infrastructure.video.models.VideoEncoderCompleted;
+import tech.willeei.admin.catalogo.infrastructure.video.models.VideoEncoderError;
+import tech.willeei.admin.catalogo.infrastructure.video.models.VideoEncoderResult;
+
+import java.util.Objects;
+
+public class VideoEncoderListener {
+
+    private static final Logger log = LoggerFactory.getLogger(VideoEncoderListener.class);
+
+    private static final String LISTENER_ID = "videoEncodedListener";
+
+    private final UpdateMediaStatusUseCase updateMediaStatusUseCase;
+
+    public VideoEncoderListener(final UpdateMediaStatusUseCase updateMediaStatusUseCase) {
+        this.updateMediaStatusUseCase = Objects.requireNonNull(updateMediaStatusUseCase);
+    }
+
+    @RabbitListener(id = LISTENER_ID, queues = "${amqp.queues.video-encoded.queue}")
+    public void onVideoEncodedMessage(@Payload final String message) {
+        final var aResult = Json.readValue(message, VideoEncoderResult.class);
+
+        if (aResult instanceof VideoEncoderCompleted dto) {
+            final var aCmd = new UpdateMediaStatusCommand(
+                    MediaStatus.COMPLETED,
+                    dto.id(),
+                    dto.video().resourceId(),
+                    dto.video().encodedVideoFolder(),
+                    dto.video().filePath()
+            );
+
+            this.updateMediaStatusUseCase.execute(aCmd);
+        } else if (aResult instanceof VideoEncoderError dto) {
+            log.error("[message:video.listener.income] [status:error] [payload:{}]", message);
+        } else {
+            log.error("[message:video.listener.income] [status:unknown] [payload:{}]", message);
+        }
+    }
+}
